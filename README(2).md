de5fdd4bdf5950fb376d60044e0dbd22919c0845
JohnPischke <100436081+JohnPischke@users.noreply.github.com>
2022-10-15 19:10:35 -0600

    finished readme

---
+++ README(2).md
@@ -0,0 +1,26 @@
+# Overview
+
+This software is a game of two player Tic-Tac-Toe in Kotlin.
+
+
+The purpose of this software was to demonstrate and become familiar with the basic principles and syntax of the Kotlin language.
+
+
+[Software Demo Video](https://youtu.be/cmTK6zB1cRA)
+
+# Development Environment
+
+* IntelliJ IDEA Community Edition 2022.2.3
+* Kotlin programming language
+
+
+# Useful Websites
+
+* [programiz](https://www.programiz.com/kotlin-programming/operators)
+* [kotlinlang.org](https://kotlinlang.org/docs/basic-syntax.html#for-loop)
+
+# Future Work
+
+* Make a computer opponent
+* Make a better draw test
+* Use the reset function 
--- test/src/main/kotlin/Main.kt
+++ test/src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+// This is my board class, it has the object that keeps track of the board and its functions.
 class Board {
 
 
@@ -12,14 +13,14 @@ class Board {
     var c3: Char = '-'
 
 
-    fun display() {
+    fun display() { // Displays the board
         println(" |a|b|c")
         println("1|$a1|$b1|$c1")
         println("2|$a2|$b2|$c2")
         println("3|$a3|$b3|$c3")
     }
 
-    fun changeboard(name:String, value:Char) {
+    fun changeboard(name:String, value:Char) { // changes the spots on the board
         if ((name == "a1") and (a1 == '-')) a1 = value
         if ((name == "a2") and (a2 == '-')) a2 = value
         if ((name == "a3") and (a3 == '-')) a3 = value
@@ -31,7 +32,7 @@ class Board {
         if ((name == "c3") and (c3 == '-')) c3 = value
 
     }
-    fun reset() {
+    fun reset() { // resets the board
         a1 = '-'
         a2 = '-'
         a3 = '-'
@@ -43,7 +44,7 @@ class Board {
         c3 = '-'
     }
 
-    fun check(player:Char):Boolean {
+    fun check(player:Char):Boolean { // checks if a player has won the game
         var win = false
         if ((a1 == player) and (a2 == player) and (a3 == player)) {win = true}
         if ((b1 == player) and (b2 == player) and (b3 == player)) {win = true}
@@ -58,7 +59,7 @@ class Board {
 
 }
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>) { // This is the main function, it is in charge of calling the game function.
     println("This is a game of two player Tic-Tac-Toe")
     println("Type the space you want to pick ie: a1, b3, c2")
     var playgame = true
@@ -73,25 +74,30 @@ fun main(args: Array<String>) {
  println("Thank you for playing my game!")
 }
 
-fun play(){
+fun play(){ // this is the play function, it is in charge of running the game loop and creating the board.
     var turn = 0
     var currentplayer = 'x'
     val board = Board()
     while(true) {
 
-        board.display()
+        board.display() // displays the board
+
         println("current turn: $turn")
         println("Player $currentplayer choose your spot.")
-        var input = readln()
-        board.changeboard(input, currentplayer)
-        if (board.check(currentplayer)) {
+
+        var input = readln() // gets the players input
+
+        board.changeboard(input, currentplayer) // changes the board based on input
+
+        if (board.check(currentplayer)) { // checks for a winner
             println("Player $currentplayer has won!")
             break
         }
-        if (currentplayer == 'x') currentplayer = 'o'
+
+        if (currentplayer == 'x') currentplayer = 'o' // changes to the next player
         else currentplayer = 'x'
 
-        turn = (turn + 1)
+        turn = (turn + 1) // increments the turn and checks for draw turn amount
         if (turn == 9) {
             println("Draw!")
             break
